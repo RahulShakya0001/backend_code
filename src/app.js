fe1aed2c16a20613fa57db49c74142b6fd47a52b
LoginPage <rahulshakya7685@gmail.com>
2025-04-02 19:03:41 +0530

    Understand route

--- src/app.js
+++ src/app.js
@@ -14,4 +14,17 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+// Routes
+import userRouter from './routes/user.routes.js';
+
+
+// Route declaration
+app.use("/api/v1/users", userRouter);
+
+// if u are using api then first please define it-> http://localhost:8000/api/v1/users -> its a standard practice that follow by company
+
+
+
 export {app};
